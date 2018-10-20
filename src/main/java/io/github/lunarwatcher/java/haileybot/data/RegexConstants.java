5a7e300ba36409aaf40ab1dea0064294185b7d55
Olivia Zoe <zoe.i2k1@gmail.com>
2018-10-20 23:50:54 +0200

    Added another site

--- src/main/java/io/github/lunarwatcher/java/haileybot/data/RegexConstants.java
+++ src/main/java/io/github/lunarwatcher/java/haileybot/data/RegexConstants.java
@@ -13,8 +13,12 @@ public class RegexConstants {
             Pattern.compile("(?i)(?:discord\\.gg/[a-z0-9]+|discordapp\\.com/invite/[a-z0-9]+|(?:pl(?:[sz]|ease)\\W*)?" +
                     "add\\W*(?:me\\W*)?(?:pl(?:[sz]|ease)\\W*)?.*\\(?(?:tag|#)\\)?\\W*\\d+(?:#\\d+)?)");
     /**
-     * Nukes twitch and rabb.it
+     * Nukes twitch and rabb.it, and twitter.
+     *
+     * All of these are legitimate sites, but they've frequently been abused by spammers to get views, follows,
+     * etc. There's also no legitimate reason to have links in usernames anyways, so this is more or less
+     * designed to fight back against the spam. 
      */
     public static final Pattern GENERAL_SPAM =
-            Pattern.compile("(?i)(?:free\\W*games\\W*at\\W*)?(?:(?:twitch\\W*tv|rabb.it)/.*(?:#\\d+)?)");
+            Pattern.compile("(?i)(?:free\\W*games|follow\\W*me)?(?:\\W*at\\W*)?(?:(?:twitch\\W*tv|rabb.it|twitter.com)/.*(?:#\\d+)?)");
 }
