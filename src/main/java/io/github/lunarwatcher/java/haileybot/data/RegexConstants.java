e722f4c1a0cbdbb845505213b766c3b8ce92ae5b
Olivia Zoe <zoe.i2k1@gmail.com>
2018-10-24 16:09:58 +0200

    More watching

--- src/main/java/io/github/lunarwatcher/java/haileybot/data/RegexConstants.java
+++ src/main/java/io/github/lunarwatcher/java/haileybot/data/RegexConstants.java
@@ -10,8 +10,10 @@ public class RegexConstants {
      * Gets rid of pesky invite and add my account bots
      */
     public static final Pattern INVITE_SPAM =
-            Pattern.compile("(?i)(?:discord\\.me.*|selly\\.gg.*|discord\\.gg/[a-z0-9]+|discordapp\\.com/invite/[a-z0-9]+|(?:pl(?:[sz]|ease)\\W*)?" +
-                    "add\\W*(?:me\\W*)?(?:pl(?:[sz]|ease)\\W*)?.*\\(?(?:tag|#|at)\\)?\\W*\\d+(?:#\\d+)?)");
+            Pattern.compile("(?i)(?:discord\\.me.*|selly\\.gg.*|discord\\.gg/[a-z0-9]+|" +
+                    "invite\\W*gg/./|discordapp\\.com/invite/[a-z0-9]+" +
+                    "|(?:pl(?:[sz]|ease)\\W*)?add\\W*(?:me\\W*)?(?:pl(?:[sz]|ease)\\W*)?.*" +
+                    "\\(?(?:tag|#|at)\\)?\\W*\\d+(?:#\\d+)?)");
     /**
      * Nukes twitch and rabb.it, and twitter.
      *
@@ -21,9 +23,10 @@ public class RegexConstants {
      */
     public static final Pattern GENERAL_SPAM =
             Pattern.compile("(?i)(?:free\\W*games|follow\\W*me)?" + // Optional spam sentences
-                    "(?:\\W*at\\W*)?" + // Appendix to the spam sentences
-                    "(?:(?:twitch\\W*tv|rabb\\W*it|twitter\\W*com|paypal\\W*me)\\W*" + // Problematic URLs
-                    ".*(?:\\W*(?:at\\W*|tag\\W*)?\\d+)?)");// optional tag
+                    "(?:\\W*at)?" + // Appendix to the spam sentences
+                    "(?:\\W*(?:twitch\\W*tv|rabb\\W*it|twitter\\W*com|paypal\\W*me).*)"
+
+            );// optional tag
 
     public static final Pattern UNCAUGHT_SPAM =
             Pattern.compile(
@@ -31,7 +34,7 @@ public class RegexConstants {
                     //@formatter:off
                     + "(" // This will contain a ton of different stuff; this is the main capture group.
                             // Begin basic spam
-                            + "(?:sell(?:ing)|tweet).*senseibin" // Usually spams selling products, mainly bots. Also links to twitter, but that's caught.
+                            + "(?:sell(?:ing)|tweet|follow).*senseibin" // Usually spams selling products, mainly bots. Also links to twitter, but that's caught.
                             + "|"
                             + "follow\\W*me\\W*(?:at|@)?.*" // catches regular "follow me" stuff. There's no legitimate reason to have this in a username anyways.
                             + "|"
