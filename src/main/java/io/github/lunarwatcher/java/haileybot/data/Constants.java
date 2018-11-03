9646dd8985010cd1d933b49197ee99ab6e196d94
Olivia Zoe <zoe.i2k1@gmail.com>
2018-11-04 00:28:28 +0100

    Version bump

--- src/main/java/io/github/lunarwatcher/java/haileybot/data/Constants.java
+++ src/main/java/io/github/lunarwatcher/java/haileybot/data/Constants.java
@@ -29,7 +29,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public final class Constants {
-    public static final String VERSION = "0.017.012 \"Confusing II\"";
+    public static final String VERSION = "0.90.1 \"Beta\"";
     public static final String TRIGGER = "h!";
     public static final boolean ALLOW_MENTION_TRIGGER = true;
     public static final String DATE_FORMAT = "dd-MM-yyyy @ HH:mm:ss";
